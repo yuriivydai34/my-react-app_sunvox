19f424168b1daeae6ed7e9d2b719e06580d7e6a5
Yurii Vydai <yuriivydai@gmail.com>
2025-07-17 15:16:48 +0300

    sunvox

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
+function useOnceCall(cb, condition = true) {
+  const isCalledRef = useRef(false);
+
+  useEffect(() => {
+    if (condition && !isCalledRef.current) {
+      isCalledRef.current = true;
+      cb();
+    }
+  }, [cb, condition]);
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatus] = useState('initial');
+
+  function updateStatus(s) { setStatus(s); console.log(s); }
+
+  useOnceCall(() => {
+    svlib.then(function (Module) {
+    svlib = Module;
+    updateStatus("SunVoxLib loading is complete");
+
+    var ver = sv_init(0, 44100, 2, 0);
+    if (ver >= 0) {
+      updateStatus("init ok");
+
+    }
+    else {
+      updateStatus("init error");
+      return;
+    }
+    sv_open_slot(0);
+    // The SunVox is initialized.
+    // Slot 0 is open and ready for use.
+    // Then you can load and play some files in this slot.
+  });
+  })
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>SunVoxLib Demo</h1>
+        Status: <font id="status">{status}</font>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
